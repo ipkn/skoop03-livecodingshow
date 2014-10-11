153dc384766af9a80e08214027bbcc9eee5e80ac
ipknHama <ipknhama@gmail.com>
2014-10-12 00:16:24 +0900

    내가(ipkn) 한 채팅의 경우 배경 색깔을 먹여서 구분되는 기능 추가

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 (function(){
     logHandler = function(x)
     {
+        if (x[0] == "chat")
+        {
+            if (x[1].indexOf("ipkn : ") == 0 || x[1].indexOf(" ") == 0)
+            {
+                x[1] = '<span style="background-color:yellow">' + x[1] + "</span>";
+            }
+        }
+
         return x;
     }
 })();
